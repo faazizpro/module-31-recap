1d52dae0a8746a3ecf518e6cec6b898fdcad5e66
faazizpro <fahadafs11@gmail.com>
2022-08-31 19:39:49 +0600

    Module 31 Recap done

---
+++ 31-9/module-summary.js
@@ -0,0 +1,13 @@
+//1. optional chaining
+//student.marks?.math
+
+//Must
+//2. map, filter, find
+
+//3. (optional): forEach, reduce(Advance)
+
+//4. class
+
+//5. (optional) inheritance, super
+
+//6. (must) prototypical Inheritance
